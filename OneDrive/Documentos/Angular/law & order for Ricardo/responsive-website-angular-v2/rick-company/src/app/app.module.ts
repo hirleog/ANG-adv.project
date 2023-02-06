6b8d237ec341fecbe1af44fde74ecad95c1deb10
hirleog <87393381+hirleog@users.noreply.github.com>
2023-02-06 12:44:33 -0300

    news

--- OneDrive/Documentos/Angular/law & order for Ricardo/responsive-website-angular-v2/rick-company/src/app/app.module.ts
+++ OneDrive/Documentos/Angular/law & order for Ricardo/responsive-website-angular-v2/rick-company/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CabecalhoModule } from './components/cabecalho/cabecalho.module';
 
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BodyComponent } from './view/body/body.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    CabecalhoModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
